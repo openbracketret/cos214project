3e1e05b5a1b1bf161508d1011a71fd0d0a299596
Luke Greenberg <lukegreen.dev@gmail.com>
2019-09-30 21:42:25 +0200

    added basic Spaceship classes

---
+++ Spaceship.h
@@ -0,0 +1,43 @@
+#ifndef SPACESHIP_H
+#define SPACESHIP_H
+
+
+class Spaceship {
+
+    public:
+        Spaceship();
+        int getDisplacement() {return displacement;};
+        void setDisplacement(int d) {displacement = d;};
+        int getPower() {return power;};
+        void setPower(int v) {power = v;};
+        int getThrust() {return thrust;};
+        void setThrust(int v) {thrust = v;};
+        int getMaxSpeed() {return maxSpeed;};
+        void setMaxSpeed(int v) {maxSpeed = v;};
+        int getStallSpeed() {return stallSpeed;};
+        void setStallSpeed(int v) {stallSpeed = v;};
+        int getCrewCapacity() {return crewCapacity;};
+        void setCrewCapacity(int p) {crewCapacity = p;};
+        int getPassengerCapacity() {return passengerCapacity;};
+        void setPassengerCapacity(int p) {passengerCapacity = p;};
+        void addFuel(int a) {fuel += a;};
+        void addEnergy(int e){energy += e;};
+        void decreaseFuel(int a){fuel -= a;};
+        void decreaseEnergy(int e){energy -= e;};
+        int getFuel() {return fuel;};
+        int getEnergy() {return energy;};
+
+    private:
+        int displacement;/**<displacement of spaceship*/
+        int power;/**<power of spaceship */
+        int thrust;/**<thrust*/
+        int maxSpeed;/**<Maxiumum Speed*/
+        int stallSpeed; /**<Stalling speed*/
+        int crewCapacity; /**<Maximum crew members*/
+        int passengerCapacity; /**<Maximum passengers*/
+        int fuel; /**<Fuel level*/
+        int energy; /**<Energy Level*/
+
+};
+
+#endif

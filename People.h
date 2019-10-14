1da99088a1857ea44711718df664cd283bd547f3
Stephen <u17049203@tuks.co.za>
2019-10-14 21:27:04 +0200

    Added people

---
+++ People.h
@@ -0,0 +1,22 @@
+#ifndef People_H
+#define People_H
+
+#include <string>
+#include <vector>
+#include "SpaceshipFactory.h"
+
+using namespace std;
+
+class People
+{
+private:
+
+
+public:
+
+	People();
+	~People();
+	
+};
+
+#endif

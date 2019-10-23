da9fad540e32a19f1af021725ac8f0bdbfff089f
Stephen <u17049203@tuks.co.za>
2019-10-23 21:37:40 +0200

    Added some doxygen comments and made a mediator for the people classes for day to day spaceship stuff

---
+++ PeopleMediator.h
@@ -0,0 +1,16 @@
+/**
+ * @file PeopleMediator.h
+*/
+#ifndef PeopleMediatior_H
+#define PeopleMediatior_H
+
+
+class PeopleMediatior {
+
+    public:
+        PeopleMediatior();
+        ~PeopleMediatior();
+
+};
+
+#endif

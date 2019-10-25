4c98118ccae32457d9f8e99a75e1fc6436eeb6f7
Luke Greenberg <lukegreen.dev@gmail.com>
2019-10-26 00:56:49 +0200

    finalise adding comments

--- SpaceshipDecorator.h
+++ SpaceshipDecorator.h
@@ -2,17 +2,22 @@
 #define SPACESHIPDECORATOR_H
 
 #include "Spaceship.h"
-
+/** @brief Spaceship Decorator class
+ */
 class SpaceshipDecorator : public Spaceship {
 
     public:
+    /** Default Constructor
+     */
         SpaceshipDecorator();
-        /**Add a component to the spaceship
+        /** Add a component to the spaceship
             @param comp The component to be added
             */
         virtual void addComponent(Spaceship* comp);
 
     protected:
+    /** default desctructor
+     */
         ~SpaceshipDecorator() {delete space;};
 
     private:

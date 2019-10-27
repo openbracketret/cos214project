9c06f4f74574cd6f4ad715891b536d999796db96
Luke Greenberg <lukegreen.dev@gmail.com>
2019-10-27 23:20:19 +0200

    begin facade

--- Facade.h
+++ Facade.h
@@ -33,13 +33,73 @@ class Facade {
      */
         vector<People*> getPeopleOnShip(Spaceship*);
 
+    /** Get a spaceship with a specific name
+     * @param name - the name of the spaceship
+     * @return Spaceship*
+     */
+        Spaceship* getSpaceshipWithName(string);
+
+    /** Begin creation of people
+     * @param number - int the number of people to create
+     */
+        void createPeople(int);
+    
+    /** Send an error message from a person to the ship they are onboard
+     * @param person - the person sending the error message
+     * @param message - string - the message being sent
+     */
+        void sendErrorMessageToShip(People*, string);
+
+    /** Get a doctor aboard a ship
+     * @param spaceship - The spaceship to get a doctor from
+     * @return People* - the doctor on the ship
+     */
+        People* getDoctorOnShip(Spaceship*);
+
+    /** Get an engineer aboard the ship
+     * @param spaceship - the spaceship to get an engineer from
+     * @return People* - The engineer
+     */
+        People* getEngineerOnShip(Spaceship*);
+
+    /** Get comms on aboard a ship
+     * @param spaceship - the ship to get a comms from
+     * @return People* - the comms
+     */
+        People* getCommsOnShip(Spaceship*);
+
+    /** Get a chief enginner aboard a ship
+     * @param spaceship - the ship to get the chief from
+     * @return People* - the chief engineer
+     */
+        People* getChiefEngineerOnShip(Spaceship*);
+
+    /** Get a fighter aboard the ship
+     * @param spaceship - the the ship to get the fighter from
+     * @return People* - the fighter from the ship
+     */
+        People* getFighterOnShip(Spaceship*);
+
+    /** Get a navigator aboard the ship
+     * @param spaceship - The ship to get the navigator from
+     * @return People* - the navigator 
+     */
+        People* getNavigatorOnShip(Spaceship*);
+
+    /** Get a captain aboard a ship
+     * @param spaceship - the spaceship to get the captin from
+     * @return People* - the captain
+     */
+        People* getCaptainOnShip(Spaceship*);
+
 
 
     private:
         vector<Spaceship*> fleet;
         Spaceship* spacestation;
-        People* commanerOfFleet;
+        People* commanderOfFleet;
         PlanetQueue* planets;
+        vector<People*> people;
 
 
 
